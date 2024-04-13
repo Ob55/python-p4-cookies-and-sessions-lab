47357ce59a4aa4b6730717fc75371a2f507b32ff
Ob55 <brian55mwangi@gmail.com>
2024-04-13 15:14:48 +0300

    done assigment

--- server/app.py
+++ server/app.py
@@ -5,6 +5,7 @@ from flask_migrate import Migrate
 
 from models import db, Article, User
 
+
 app = Flask(__name__)
 app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
